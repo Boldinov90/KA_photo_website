3eeb3c9532d76d5fe2df2cbc8275176a2e61da00
Boldinov90 <detroid90@mail.ru>
2020-12-11 14:08:08 +0300

    s90

--- js/main.js
+++ js/main.js
@@ -39,6 +39,7 @@ window.onscroll = function() {
 }
 
 // =========== Навигация по сайту =============
+// ======================================================================================
 const anchors = document.querySelectorAll('a[href*= "#"]')
 for(let anchor of anchors){
     anchor.addEventListener("click", function(event){
@@ -54,8 +55,8 @@ for(let anchor of anchors){
     })
 }
 
-// // ===============  Подключение сортировки элементов в галерее по клику меню ============
-// // ======================================================================================
+// ===============  Подключение сортировки элементов в галерее по клику меню ============
+// ======================================================================================
 document.querySelector(".gallery-nav__items-list").addEventListener("click", function(e) {
     // Отмена стандартного поведения браузера
     e.preventDefault();
@@ -73,6 +74,7 @@ var mixer = mixitup('.portfolio__gallery-items', config);
 
 
 // Раскрытие всего контента в галерее
+// ======================================================================================
 document.querySelector('.portfolio__btn-lookAll').addEventListener("click", function(e) {
     // Отмена стандартного поведения браузера
     e.preventDefault();
@@ -82,13 +84,19 @@ document.querySelector('.portfolio__btn-lookAll').addEventListener("click", func
     document.querySelector('.portfolio__btn-lookAll').classList.add("hidden-btn");
 
 });
+
 // Cкрытие всего контента в галерее
+// ======================================================================================
 document.querySelector('.portfolio__btn-back').addEventListener("click", function(e) {
     // Отмена стандартного поведения браузера
     e.preventDefault();
     document.querySelector('.portfolio__gallery-items').classList.remove("gallery-items__allContent");
     document.querySelector('.portfolio__btn-lookAll').classList.remove("hidden-btn");
     document.querySelector('.portfolio__btn-back').classList.add("hidden-btn");
+    document.getElementById("portfolio").scrollIntoView({
+        behavior: "smooth",
+        block: "start" 
+        });
 });
 
 // ====================     Раскрытие и скрытие всего контента в "О СЕБЕ"    ==================
@@ -224,28 +232,3 @@ $(document).ready(function() {
     });
 });
 
-// Отправка данных из формы обратной связи на почту
-$(document).ready(function(){
-	$("form").submit(function() { // Событие отправки с формы
-		var form_data = $(this).serialize(); // Собираем данные из полей
-		$.ajax({
-			type: "POST", // Метод отправки
-			url: "sendform.php", // Путь к PHP обработчику sendform.php
-			data: form_data,
-			success: function() {
-				$('.popup').addClass('active');
-				$('.thanks').addClass('active');
-			}
-		});
-		event.preventDefault();
-		document.querySelector(".form__body").style.display = "none";
-		document.querySelector(".form__body-reaction").style.display = "flex";
-		setTimeout(() => {  
-			document.querySelector(".form__body").style.display = "flex";
-			document.querySelector(".form__body-reaction").style.display = "none";
-			document.querySelector("#name").value = "";
-			document.querySelector("#phone").value = "";
-			document.querySelector("#checkbox").checked = false;
-        }, 2500);
-	});
-});
--- libs/form.js
+++ libs/form.js
@@ -1,24 +1,24 @@
-// $(document).ready(function(){
-// 	$("form").submit(function() { // Событие отправки с формы
-// 		var form_data = $(this).serialize(); // Собираем данные из полей
-// 		$.ajax({
-// 			type: "POST", // Метод отправки
-// 			url: "sendform.php", // Путь к PHP обработчику sendform.php
-// 			data: form_data,
-// 			success: function() {
-// 				$('.popup').addClass('active');
-// 				$('.thanks').addClass('active');
-// 			}
-// 		});
-// 		event.preventDefault();
-// 		document.querySelector(".form__body").style.display = "none";
-// 		document.querySelector(".form__body-reaction").style.display = "flex";
-// 		setTimeout(() => {  
-// 			document.querySelector(".form__body").style.display = "flex";
-// 			document.querySelector(".form__body-reaction").style.display = "none";
-// 			document.querySelector("#name").value = "";
-// 			document.querySelector("#phone").value = "";
-// 			document.querySelector("#checkbox").checked = false;
-//         }, 2500);
-// 	});
-// });
+$(document).ready(function(){
+	$("form").submit(function() { // Событие отправки с формы
+		var form_data = $(this).serialize(); // Собираем данные из полей
+		$.ajax({
+			type: "POST", // Метод отправки
+			url: "sendform.php", // Путь к PHP обработчику sendform.php
+			data: form_data,
+			success: function() {
+				$('.popup').addClass('active');
+				$('.thanks').addClass('active');
+			}
+		});
+		event.preventDefault();
+		document.querySelector(".form__body").style.display = "none";
+		document.querySelector(".form__body-reaction").style.display = "flex";
+		setTimeout(() => {  
+			document.querySelector(".form__body").style.display = "flex";
+			document.querySelector(".form__body-reaction").style.display = "none";
+			document.querySelector("#name").value = "";
+			document.querySelector("#phone").value = "";
+			document.querySelector("#checkbox").checked = false;
+        }, 2500);
+	});
+});
